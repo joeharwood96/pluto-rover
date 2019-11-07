23198b316228c85adc970539bfc4596fc983ba51
Joseph Harwood <joseph.harwood@ibm.com>
2019-11-07 20:04:01 +0000

    Added moveBack method and test to ensure B command works

--- plutoRover.py
+++ plutoRover.py
@@ -16,8 +16,17 @@ class plutoRover:
 
         return x, y, direction
 
-    # def moveBackward(self, direction, x, y):
+    def moveBackward(self, direction, x, y):
+        if direction == "N":
+            y -= 1
+        elif direction == "S":
+            y += 1
+        elif direction == "E":
+            x -= 1
+        else:
+            x += 1
 
+        return x, y, direction
 
     # def rotateLeft(self, direction):
     
@@ -26,10 +35,38 @@ class plutoRover:
     
 
     # def checkEdges(self, grid, finalPosition):
+
+
+    def auth(self, command, direction, x, y):
+        direc = {
+            "N",
+            "S",
+            "E",
+            "W"
+        }
+
+        com = {
+            "F",
+            "B",
+            "L",
+            "R"
+        }
+
+        if direction is not None and command is not None:
+            if direction in direc and command in com:
+                if command == "F":
+                    x, y, direction =  self.moveForward(direction, x, y)
+                    finalPosition = (x, y, direction)
+                elif command == "B":
+                    x, y, direction = self.moveBackward(direction, x, y)
+                    finalPosition = (x, y, direction)
+
+        return finalPosition
     
          
     def main(self):
         currentPos = self.position
+        command = self.command
 
         if len(currentPos) == 3:
             currentDir = currentPos[2]
@@ -38,7 +75,7 @@ class plutoRover:
         else:
             return print("No position found")
 
-        return self.moveForward(currentDir, x, y)
+        return self.auth(command, currentDir, x, y)
             
 
-print(plutoRover([0, 0, "E"], "F", [100, 100]).main())
+print(plutoRover([2, 0, "E"], "B", [100, 100]).main())
--- plutoRoverTest.py
+++ plutoRoverTest.py
@@ -3,8 +3,12 @@ from plutoRover import plutoRover
 
 class TestPlutoRover(unittest.TestCase):
 
-    def test_x_coordinate(self):
+    def test_x_coordinate_forward(self):
         self.assertEqual(plutoRover([0, 0, "E"], "F", [100, 100]).main(), (1, 0, 'E'))
+
+    def test_x_coordinate_backwards(self):
+        self.assertEqual(plutoRover([2, 0, "E"], "B", [100, 100]).main(), (1, 0, 'E'))
+    
 
 if __name__ == '__main__':
     unittest.main()
